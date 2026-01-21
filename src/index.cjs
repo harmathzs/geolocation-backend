999b916e0e7a61bc7736b4244ca96246c4b49da5
Zsolt Harmath <harmathzs@gmail.com>
2026-01-21 08:07:37 +0100

    Create Node project

---
+++ src/index.cjs
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+// endpoints
+app.get('/hello', (req, res)=>{
+    return res.status(200).json({message: "Hello gypsies!"})
+})
+
+const port = 3333
+var actualPort = process.env.PORT || port
+app.listen(actualPort, (err)=>{
+    console.log('Backend runs on port', actualPort)
+})
